c5a35d9f06f9b2b13f6b5d8b10871d6198d3b64a
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-09-05 13:39:40 +0200

    Order class first iteration created

---
+++ Webshop/Model/Order.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Model
+{
+    class Order
+    {
+		private int orderId;
+
+		public int OrderId
+		{
+			get { return orderId; }
+			set { orderId = value; }
+		}
+
+		
+		public enum delivStat {OrderPlaced, OnWay, Delivered }
+		private string deliveryStatus;
+
+		public string DeliveryStatus
+		{
+			get { return deliveryStatus; }
+			set { deliveryStatus = value; }
+		}
+
+
+		public enum payMethod {Card, Cash, MobilePay}
+		private string paymentMethod;
+
+		public string PaymentMethod
+		{
+			get { return paymentMethod; }
+			set { paymentMethod = value; }
+		}
+
+		private bool paid;
+
+		public bool Paid
+		{
+			get { return paid; }
+			set { paid = value; }
+		}
+
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+
+
+		Order(int orderId, string deliveryStatus, string paymentMethod, bool paid, string email)
+		{
+			OrderId = orderId;
+			DeliveryStatus = deliveryStatus;
+			PaymentMethod = paymentMethod;
+			Paid = paid;
+			Email = email;
+		}
+
+
+		public int PK_OrderId()
+		{
+			throw new NotImplementedException();
+		}
+
+
+	}
+}
